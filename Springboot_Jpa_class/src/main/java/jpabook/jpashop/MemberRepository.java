5f049fa566eba18c8a13da224b9c0a7c06dd5b45
bsh6463 <bsh9510@gmail.com>
2021-08-05 17:24:44 +0900

    210805 project setting

---
+++ Springboot_Jpa_class/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext //영속성 컨텍스트를 스프링 부트가 주입!해줌, 생성 필요없어짐 ㄱㅇㄷ
+   private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
---
+++ Springboot_Jpa_class/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class) //Junit4 , 스프링 관련 test
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("userA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.find(savedId);
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); //==비교
+    }
+
+}
