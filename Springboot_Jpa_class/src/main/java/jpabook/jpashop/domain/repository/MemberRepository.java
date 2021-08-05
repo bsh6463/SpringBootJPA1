7a6aba0c52dfcdf351008b619196befba2cd1ab5
bsh6463 <bsh9510@gmail.com>
2021-08-06 00:56:20 +0900

    210806 Member Repository

---
+++ Springboot_Jpa_class/src/main/java/jpabook/jpashop/domain/repository/MemberRepository.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.domain.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository //스프링 빈으로 등록, 컴포넌트 스캔.
+public class MemberRepository {
+
+    // JPA 표준 annotaion
+    //스프링이 entitymanager 만들어저 주입해줌.
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(){
+        //JPQL 사용 createQuery(JPQL, 반환타입)
+       return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name) //:name이랑 binding됨.
+                .getResultList();
+    }
+
+}
