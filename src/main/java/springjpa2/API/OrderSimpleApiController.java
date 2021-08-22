9815383c0d39e5f0ae2ee7e660270f28809cc359
bsh6463 <bsh9510@gmail.com>
2021-08-22 23:04:24 +0900

    210822 N+1, LAZY LOADING

--- src/main/java/springjpa2/API/OrderSimpleApiController.java
+++ src/main/java/springjpa2/API/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package springjpa2.API;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springjpa2.domain.Address;
 import springjpa2.domain.Order;
+import springjpa2.domain.OrderStatus;
 import springjpa2.repository.OrderRepository;
 import springjpa2.repository.OrderSearch;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 연관관계(x to One)
@@ -29,5 +34,35 @@ public class OrderSimpleApiController {
             order.getDelivery();
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimPleOrderDto> odersV2(){
+
+        //ORDER 2개 조회됨.
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimPleOrderDto> result = orders.stream()
+                .map(o -> new SimPleOrderDto(o)) //map은 a를 b로 바꾸는 것.
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    private class SimPleOrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimPleOrderDto(Order order){
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+        }
     }
 }
